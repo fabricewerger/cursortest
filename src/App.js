4757dd2680217177ea777f1f7a722fdbfd988ef9
fabricewerger <fabricewerger@icloud.com>
2023-10-09 02:41:02 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,195 @@
+import React from "react";
+import CursorProvider from './Providers/cursorProvider';
+import Button from './Button';
+import Sides from './Sides';
+import "./styles.css";
+
+export default function App() {
+  return (
+    <CursorProvider>
+      <div className="App">
+        asddsd
+        <br />
+        asddsd
+        <br />
+        <Button href="#">This is a link</Button>
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        <Button type="button">This is a Button</Button>
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <Sides/>
+        <br />
+        vasddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        vasddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        vasddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        vasddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        vasddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        vasddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        vasddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        vasddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        vasddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        asddsd
+        <br />
+        vasddsd
+        <br />
+      </div>
+    </CursorProvider>
+  );
+}
